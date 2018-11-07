3d4735d740dc54169851d37f52c8834fba8be770
Hugolifes <rach.rc36@gmail.com>
2018-11-07 02:46:36 -0700

    Update JavaScript/Scripts.js

--- JavaScript/Scripts.js
+++ JavaScript/Scripts.js
@@ -6,12 +6,12 @@ function myFunction() {
     document.getElementById("demo").innerHTML = esp + "<br>" + esp2;
 }
 
-
+/*
 var disqus_config = function () {
 this.page.url = 'https://buenasabiduria.disqus.com/embed.js' ;  // Replace PAGE_URL with your page's canonical URL variable
 this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
 };
-
+*/
 (function() {
 var d = document, s = d.createElement('script');
 s.src = 'https://buenasabiduria.disqus.com/embed.js';
