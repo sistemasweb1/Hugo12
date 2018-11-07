fc455786dc0c168424778761316912cb2f8bec40
Hugolifes <rach.rc36@gmail.com>
2018-11-07 02:55:09 -0700

    ads
    ...

--- JavaScript/Scripts.js
+++ JavaScript/Scripts.js
@@ -1,3 +1,21 @@
 
 function myFunction() {
-    document.getElementById("demo").innerHTML = "La Guerra esencial consiste en la lucha entre los partidarios";
+	var esp = "La Guerra esencial consiste en la lucha entre los partidarios de le materia ";
+	var esp2 = "contra los partidarios del espiritu.";
+
+    document.getElementById("demo").innerHTML = esp + "<br>" + esp2;
+}
+
+/*
+var disqus_config = function () {
+this.page.url = 'https://buenasabiduria.disqus.com/embed.js' ;  // Replace PAGE_URL with your page's canonical URL variable
+this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+};
+*/
+(function() {
+var d = document, s = d.createElement('script');
+s.src = 'https://buenasabiduria.disqus.com/embed.js';
+s.setAttribute('data-timestamp', +new Date());
+(d.head || d.body).appendChild(s);
+})();
+
