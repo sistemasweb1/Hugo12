a57c3e17fb107601bd0dda6b4dd23b8409c0d3f3
Hugolifes <rach.rc36@gmail.com>
2018-11-21 19:19:58 -0700

    exercises
    javascript's examples

--- JavaScript/Scripts.js
+++ JavaScript/Scripts.js
@@ -19,5 +19,16 @@ s.setAttribute('data-timestamp', +new Date());
 (d.head || d.body).appendChild(s);
 })();
 
+function mostrar() {
+    document.getElementById("sidebar").style.width = "300px";
+    document.getElementById("contenido").style.marginLeft = "300px";
+    document.getElementById("abrir").style.display = "none";
+    document.getElementById("cerrar").style.display = "inline";
+}
 
-
+function ocultar() {
+    document.getElementById("sidebar").style.width = "0";
+    document.getElementById("contenido").style.marginLeft = "0";
+    document.getElementById("abrir").style.display = "inline";
+    document.getElementById("cerrar").style.display = "none";
+}
---
+++ JavaScript/exercices.js
@@ -0,0 +1,200 @@
+#1
+
+var CarName = "volvo";
+
+
+#2
+var x = 50;
+
+
+#3
+var x= 5;
+var y = 10;
+document.getElementById("demo").innerHTML = x + y;
+
+#4
+
+var x = 5;
+var y = 10;
+var z = x + y;
+alert(z);	
+
+#5
+
+var  firstName= "John", lastName = "Doe", age = 35 ;
+
+#6
+
+alert(10*5);
+
+#7
+
+alert(10/2);
+
+#8
+
+alert(15%9);
+
+#9
+
+x = 10;
+y = 5;
+x += y;
+
+#10
+
+x = 10;
+y = 5;
+x *= y;
+
+#11
+
+function myFunction() {
+  alert("Hello World!");
+}
+myFunction();
+
+#12
+function myFunction(){
+	alert("Hello World!");
+}
+
+#13
+function myFunction(){
+	document.getElementById("demo").innerHTML="Hello";
+}
+
+
+#14
+var person = {
+  firstName: "John",
+  lastName: "Doe"
+};
+
+alert(person.firstName);
+
+#15
+var person = {
+  firstName: "John",
+  lastName: "Doe",
+  country: "Norway"
+};
+
+#16
+var person = {
+  name: "John", age:50
+};
+
+alert(person.name + "is" + person.age);
+
+#17
+//<button onclick=alert(Hello)>Click me.</button>
+
+#18
+//<button onclick="myFunction">Click Me.</button>
+
+#19
+//<div onmouseover="this.style.backgroundColor='red'">myDIV.</div>
+
+#20
+var txt = "Hello World!";
+var x = txt.length;
+alert(x);
+
+#21
+
+var txt = "we are \"vikings\"";
+alert(txt);
+
+#22
+
+var str1 = "Hello ";
+var str2 = "World!";
+alert(str1 + str2);
+
+#23
+
+var txt = "abcdefghijklm";
+var pos = txt.indexOf("h");
+
+#24
+var	 txt="i can eat bananas all day";
+var x=txt.slice(10,17);
+
+#25
+var txt ="Hello World";
+txt= txt.replace("Hello","Welcome");
+
+#26
+var txt ="Hello World";
+txt = txt.toUpperCase();
+
+#27
+var txt = "Hello World";
+txt = txt.toLowerCase();
+
+#28
+var str1 = "Hello ";
+var str2 = "World!";
+var result = str1.concat(str2);
+
+#29
+var cars = ["Saab", "Volvo", "BMW"];
+var x = cars[1];
+
+#30
+var cars = ["Volvo", "Jeep", "Mercedes"];
+cars[0]= "Ford";
+
+#31
+var cars = ["Volvo", "Jeep", "Mercedes"];
+alert(cars.length);
+
+#32
+var fruits = ["Banana", "Orange", "Apple"];
+fruits.pop();
+
+#33
+var fruits = ["Banana", "Orange", "Apple"];
+fruits.push("Kiwi");
+
+#34
+
+var fruits = ["Banana", "Orange", "Apple", "Kiwi"];
+fruits.splice(1, 2);
+
+#35
+
+var girls = ["Cecilie", "Lone"];
+var boys = ["Emil", "Tobias", "Linus"];
+var children = ;
+
+#36
+var girls = ["Cecilie", "Lone"];
+var boys = ["Emil", "Tobias", "Linus"];
+var children = girls.concat(boys);
+
+#37
+var fruits = ["Banana", "Orange", "Apple", "Kiwi"];
+fruits.sort();
+
+#38
+var fruits = ["Banana", "Orange", "Apple", "Kiwi"];
+fruits.sort();
+
+#39
+
+var d = new Date();
+year = d.getFullYear() ;
+
+#40
+
+var d = new Date();
+month = d.getMonth();
+
+
+		
+
+
+
+
